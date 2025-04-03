e70e2329dcddae24eb41aa8032689d37cf1cae4e
hnk005 <huynhnamkha512020@gmail.com>
2025-04-03 17:38:15 +0700

    update: demo

---
+++ demo/assets/useChatHistory-CNVuEuXE.js
@@ -0,0 +1,1 @@
+import{r as o,a3 as s}from"./index-B7wwjnbK.js";const e="/healthybot/assets/logo-X5r45KDl.png",a=()=>{const t=o.useContext(s);if(!t)throw new Error("useHistoryChat must be used within HistoryChatProvider");return t};export{e as l,a as u};
